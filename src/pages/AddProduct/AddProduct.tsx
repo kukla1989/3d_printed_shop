8658baebf81f034752aeba332b5416249267e51f
kukla <kukla.1989@ukr.net>
2026-08-23 12:31:56 +0300

    add section 'add product via link from shafa' to AddProduct

--- src/pages/AddProduct/AddProduct.tsx
+++ src/pages/AddProduct/AddProduct.tsx
@@ -3,7 +3,22 @@ import styles from "./AddProduct.module.scss"
 function AddProduct() {
   return (
     <div className={styles.addProduct}>
-      add_product
+      <div className={styles.section}>
+        <div className={styles.title}>
+          добавить виріб через посилання із shafa
+        </div>
+
+        <div className={styles.block}>
+          <input type="text" className={styles.input}
+                 placeholder="посилання на shafa" />
+
+          <button className={styles.button}>добавить</button>
+        </div>
+      </div>
+
+      <div className={styles.title}>
+        добавить виріб вручну
+      </div>
     </div>
   )
 }
